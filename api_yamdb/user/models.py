73f106bab9d33d50299cef5976031452facce98e
Aleksandr <smirnow-2012@mail.ru>
2022-10-03 18:58:38 +0300

    add models user

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+ROLES = {
+    {'user', 'User'},
+    {'moderator', 'Moderator'},
+    {'admin', 'Admin'}
+}
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        blank=False,
+        max_length=254,
+        unique=True
+    )
+    bio = models.TextField(blank=True)
+    role = models.TextField(blank=True, choices=ROLES, default='user')
+
