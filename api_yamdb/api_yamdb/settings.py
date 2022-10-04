57cdcfe0099dd9e6cbf555aa44057862c5fdb6dd
Aleksandr <smirnow-2012@mail.ru>
2022-10-05 00:04:30 +0300

    Correction of the model. Adding the first name, last name and token fields. Fixing access and urls

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -24,10 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'rest_framework_simplejwt',
-    'users.apps.UserConfig',
-    'api'
 ]
 
 MIDDLEWARE = [
