d741ec1cced5e33dbf9f3061f44d1c58991a1433
Aleksandr <smirnow-2012@mail.ru>
2022-10-04 12:17:58 +0300

    Serialization has been added, the network view and token verification function are described, routers by urls have been updated

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,1 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from api.views import UserViewSet, get_jwt_token
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+
+router.register('users', UserViewSet, basename='users')
+
+auth_urlpatterns = [
+    path('token/', get_jwt_token)
+]
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/', include(auth_urlpatterns)),
+]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, filters, status, permissions
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+from .permissions import (IsAdminModeratorAuthorOrReadOnly,
+                          IsAdminOrSuperuserOrReadOnly,
+                          IsAdmin)
+from .serializers import UserSerializers, UserEditSerializer, TokenSerializer
+from users.models import User
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    #permission_classes =
+    filter_backends = (filters.SearchFilter,)
+    filters_fields = ('username',)
+    search_fields = ('username',)
+    lookup_field = 'username'
+
+    @action(methods=['get', 'patch'],
+            detail=False,
+            serializer_class=UserEditSerializer,
+            url_path='me')
+    def user_self_profile(self, request):
+        user = request.user
+        if request.method == 'GET':
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(
+            user,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def get_jwt_token(request):
+    serializer = TokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = get_object_or_404(User,
+                             username=serializer.validated_data['username'])
+    if default_token_generator.check_token(
+        user, serializer.validated_data['confirmation_code']
+    ):
+        token = AccessToken.for_user(user)
+        return Response({'token': str(token)}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
