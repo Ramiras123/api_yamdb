6728ff236aa3fd81ce3eaaf306d8cb93ba1b6f25
Артем Римша <artem-rimsha@mail.ru>
2022-10-05 20:14:37 +0300

    Коммит

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, permissions, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+from .mixins import ListCreateDestroyViewSet
+from .filters import TitlesFilter
+from .permissions import IsAdminOrReadOnly
+from .serializers import (CategorySerializer,
+                          GenreSerializer, ReadOnlyTitleSerializer,
+                          TitleSerializer)
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
+    lookup_field = "slug"
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
+    lookup_field = "slug"
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitlesFilter
+
+    def get_serializer_class(self):
+        if self.action in ("retrieve", "list"):
+            return ReadOnlyTitleSerializer
+        return TitleSerializer
