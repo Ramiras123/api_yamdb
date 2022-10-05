f4b1f1b920325a8d60d1ddcea5d6f32b34b0baf6
Ekaterina Rusakova <katerina@neiron.ru>
2022-10-05 19:19:11 +0300

    basic import

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
+DATA_DIR = os.path.join(BASE_DIR, "static", "data")
 
 AUTH_USER_MODEL = 'users.User'
 
